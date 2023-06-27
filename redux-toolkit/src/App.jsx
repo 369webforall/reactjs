1897aa2d04b9fd12cffa4adf5efebb50eb72bbd0
kabitaremotework <104657984+kabitaremotework@users.noreply.github.com>
2023-06-27 15:16:44 +0545

    redux

---
+++ redux-toolkit/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Counter from './components/Counter'
+const App = () => {
+  return (
+    <div className='container mx-auto mt-10'>
+      <Counter />
+    </div>
+  )
+}
+
+export default App
